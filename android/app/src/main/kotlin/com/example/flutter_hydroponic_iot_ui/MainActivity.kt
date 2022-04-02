1a7beec4a6eefc696667a6328995eb66e817aeb6
stupidwolfy <dsl526b@gmail.com>
2022-04-02 13:02:28 +0700

    / update android env

--- android/app/src/main/kotlin/com/example/flutter_hydroponic_iot_ui/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_hydroponic_iot_ui/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.flutter_hydroponic_iot_ui
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
